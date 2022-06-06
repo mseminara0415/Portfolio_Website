c63081771754ce6cc5666d1dc7ba0cd25f10fca6
Matt Seminara <matthewseminara@gmail.com>
2022-06-06 01:37:23 -0700

    update docker compose to create data volume which takes raw api json data and saves it.

--- docker/dags/tracker.py
+++ docker/dags/tracker.py
@@ -4,16 +4,19 @@ from datetime import datetime
 from io import BytesIO
 import json
 import logging
+import os
 import pandas as pd
+from pathlib import Path
 import requests
 import re
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.bash import BashOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 
-def load_raw_iss_data_to_s3(norad_id: int = 25544, units: str = "miles", is_tle:bool = False) -> dict:
+def download_satellite_data(norad_id: int = 25544, units: str = "miles", is_tle:bool = False) -> dict:
     '''_summary_
 
     Parameters
@@ -31,43 +34,40 @@ def load_raw_iss_data_to_s3(norad_id: int = 25544, units: str = "miles", is_tle:
         When this parameter is False we instead return positioning data (lat, long, altitude, etc..)
     '''
 
-    # Set s3 client
-    s3 = boto3.client(
-        's3'
-    )
-
-    # Get todays data, which will be used for partitioning folders within bucket
-    todays_date = datetime.today().strftime(r"%Y-%m-%d")
-
-    # Set destination bucket for raw satallite tracking data
-    bucket_name = "satellite-tracker-raw"
-    
-
     # If we want to return positioning data
     if not is_tle:
         # Get position data from API
         api_url = f"https://api.wheretheiss.at/v1/satellites/{norad_id}?units={units}&?timestamp"
         iss_data = requests.get(api_url).json()
-
-        # Get the s3 objects timestamp based on the requested datatype
-        s3.put_object(
-            Bucket=bucket_name,
-            Key=f"position-data/{todays_date}/{datetime.now()}-position.json",
-            Body=json.dumps(iss_data)
-        )
+        satellite_data_type = "position"
 
     # If we want to return orbital data
     elif is_tle:
         # Get tle data from API
         api_url_tle = f"https://api.wheretheiss.at/v1/satellites/{norad_id}/tles"
-        iss_data = requests.get(api_url_tle).json()
+        iss_data = requests.get(api_url).json()
+        satellite_data_type = "tle"
+    
+    timestamp_value = datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
+    
+    file_path = f"/usr/local/airflow/satellite_tracking/data/{satellite_data_type}_raw-{timestamp_value}.json"
+    file_path_2 = Path(r'/usr/local/airflow/satellite_tracking/data',satellite_data_type+'-'+timestamp_value+'.json')
 
-        # Get the s3 objects timestamp based on the requested datatype
-        s3.put_object(
-            Bucket=bucket_name,
-            Key=f"tle-data/{todays_date}/{datetime.now()}-tle.json",
-            Body=json.dumps(iss_data)
-        )
+    with open(file_path_2,'w') as f:
+        json.dump(iss_data,f)
+
+def upload_to_s3(filename: str, key: str, bucket_name: str) -> None:
+    hook = S3Hook('s3_conn_id')
+    hook.load_file(
+        filename=filename,
+        key=key,
+        bucket_name=bucket_name
+    )
+
+# def pre_process():
+#     f = open("/usr/local/airflow/satellite_tracking/data/example.json", "w")
+#     f.write("key':'value'")
+#     f.close()
 
 dag = DAG(
 dag_id="my_dag",
@@ -76,12 +76,32 @@ catchup=False,
 schedule_interval='*/10 * * * * *'    
 )
 
-load_raw_position_data = PythonOperator(
-    task_id="load_raw_iss_position_data_to_s3",
-    python_callable=load_raw_iss_data_to_s3,
+# check_file = BashOperator(
+#         task_id="check_file",
+#         bash_command="echo Hi > /usr/local/airflow/satellite_tracking/data/example.json ",
+#         dag=dag
+#     )
+
+pre_processing = PythonOperator(
+    task_id="pre_process",
+    python_callable=download_satellite_data,
     dag=dag
-    
 )
+
+pre_processing
+
+# load_raw_position_data = PythonOperator(
+#     task_id="download_satellite_data",
+#     python_callable=download_satellite_data,
+#     dag=dag    
+# )
+
+# load_raw_position_data = PythonOperator(
+#     task_id="load_raw_iss_position_data_to_s3",
+#     python_callable=load_raw_iss_data_to_s3,
+#     dag=dag
+    
+# )
 
 # load_raw_tle_data = PythonOperator(
 #     task_id="load_raw_iss_tle_data_to_s3",
@@ -91,22 +111,3 @@ load_raw_position_data = PythonOperator(
 # )
 
 # get_data_from_api >> api_data_to_dataframe >> raw_data_to_s3
-
-
-# Steps of datapipeline:
-'''
-Steps of datapipline:
-
-1. Pull data from API using requests. Save as json file in s3 bucket.
-
-
-2.
-
-
-3.
-
-
-4.
-
-
-'''
