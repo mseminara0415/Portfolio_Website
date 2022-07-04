7a30308595323f21be770a341e979b1508ec4a0b
Matt Seminara <matthewseminara@gmail.com>
2022-07-04 14:47:24 -0700

    testing

---
+++ dbt/target/run/satellite_tracker/models/example/my_second_dbt_model.sql
@@ -0,0 +1,8 @@
+
+  create view "portfolio_website"."analytics"."my_second_dbt_model__dbt_tmp" as (
+    -- Use the `ref` function to select from other models
+
+select *
+from "portfolio_website"."analytics"."my_first_dbt_model"
+where id = 1
+  );
