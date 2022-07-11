4ac0c0b39715bfb2f86a5ecb3e075e4282dc98a6
Matt Seminara <matthewseminara@gmail.com>
2022-07-10 19:18:16 -0700

    dbt help support instructions

--- dbt/target/run/satellite_tracker/models/example/my_second_dbt_model.sql
+++
@@ -1,8 +0,0 @@
-
-  create view "portfolio_website"."staging"."my_second_dbt_model__dbt_tmp" as (
-    -- Use the `ref` function to select from other models
-
-select *
-from "portfolio_website"."staging"."my_first_dbt_model"
-where id = 1
-  );
